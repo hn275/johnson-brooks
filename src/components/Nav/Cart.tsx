46609929b1c26c6f65de04bd115d21b13bd1d9ac
Hal <haln_01@proton.me>
2023-04-23 14:36:41 -0700

    feat: logos

---
+++ src/components/Nav/Cart.tsx
@@ -0,0 +1,10 @@
+import { BsCart2 } from "react-icons/bs";
+
+export function Cart() {
+  return (
+    <BsCart2
+      size={20}
+      className="text-slate-500 md:text-slate-400 md:hover:text-slate-600 transition-colors cursor-pointer"
+    />
+  );
+}
