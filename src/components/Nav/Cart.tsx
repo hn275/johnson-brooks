121ef9eb32283e12206b4c16c2bb3098ff7938c1
Hal <haln_01@proton.me>
2023-05-04 00:22:51 -0700

    feat: nav bar

--- src/components/Nav/Cart.tsx
+++ src/components/Nav/Cart.tsx
@@ -1,11 +1,16 @@
 import icons from "react-icons/bs/index";
+import cx from "classnames";
 
 export function Cart() {
   const { BsCart2 } = icons;
   return (
     <BsCart2
       size={20}
-      className="text-slate-500 md:text-slate-400 md:hover:text-slate-600 transition-colors cursor-pointer"
+      className={cx(
+        "text-slate-500 md:text-slate-400",
+        "md:hover:text-slate-600",
+        "transition-colors cursor-pointer",
+      )}
     />
   );
 }
