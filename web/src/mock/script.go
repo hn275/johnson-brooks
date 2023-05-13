95f8b307bf22ce3de06a2de0fb1b14df6a9c0423
hal <haln_01@proton.me>
2023-05-12 18:09:51 -0700

    feat: add to cart

--- web/src/mock/script.go
+++ web/src/mock/script.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
 type MockData struct {
+	ID          string  `json:"id"`
 	Src         string  `json:"src"`
 	Title       string  `json:"title"`
 	Material    string  `json:"material"`
@@ -33,6 +35,7 @@ func main() {
 	var data []MockData
 	for i := 0; i < 10; i++ {
 		data = append(data, MockData{
+			ID:          fmt.Sprintf("%d", i),
 			Title:       "Mono rail",
 			Src:         buf.String(),
 			Material:    "Plastic",
