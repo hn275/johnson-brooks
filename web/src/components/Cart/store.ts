6954dfc81e1cb03b16e5174bd6f93958bc68ccbc
hal <haln_01@proton.me>
2023-05-12 17:45:06 -0700

    feat: cart on nav

--- web/src/components/Cart/store.ts
+++ web/src/components/Cart/store.ts
@@ -8,3 +8,17 @@ export type CartItem = {
 };
 
 export const cartItem = store.map<Record<string, CartItem>>({});
+
+export function addToCart(id: string, item: CartItem) {
+  return () => {
+    const product = cartItem.get()[id];
+
+    if (product) {
+      const quantity = product.quantity + item.quantity;
+      cartItem.setKey(id, { ...product, quantity });
+      return;
+    }
+
+    cartItem.setKey(id, item);
+  };
+}
