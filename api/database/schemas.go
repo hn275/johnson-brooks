bb1b7a8ee74f556673a05f06a0ccc5377ec79288
Hal <haln_01@proton.me>
2023-05-13 11:20:24 -0700

    style: reformat struct comment

--- api/database/schemas.go
+++ api/database/schemas.go
@@ -2,18 +2,17 @@ package database
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// NOTE: `Thumbnail` is a base64 encoded string of bytearray of an image
+// PRODUCT
 type Product struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Title       string             `json:"title" bson:"title"`
-	Thumbnail   string             `json:"thumbnail" bson:"thumbnail"`
+	Thumbnail   string             `json:"thumbnail" bson:"thumbnail"` // base64 encoding of bytearray
 	Description string             `json:"description" bson:"description"`
 	Material    string             `json:"material" bson:"material"`
 	Price       float32            `json:"price" bson:"price"`
 	Inventory   uint16             `json:"inventory" bson:"inventory"`
 }
 
-// CLIMBING
 type ClimbingProduct struct {
 	Product  `bson:",inline"`
 	Variants []ClimbingVariant `json:"variants" bson:"variants"`
@@ -25,7 +24,6 @@ type ClimbingVariant struct {
 	Color     string `json:"color" bson:"color"`
 }
 
-// BOARDS
 type BoardProduct struct {
 	Product `bson:",inline"`
 }
