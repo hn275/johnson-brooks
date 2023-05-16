a22ce91978ea41ae962df03549e38f2141e06a1a
Hal <haln_01@proton.me>
2023-05-16 01:04:55 -0700

    feat[wip]:serializer

--- api/database/schemas.go
+++ api/database/schemas.go
@@ -1,13 +1,19 @@
 package database
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"errors"
+	"jb/lib/img"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // PRODUCT
 type Product struct {
-	ID            primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	ObjectID      primitive.ObjectID `json:"-" bson:"_id,omitempty"`
+	ID            string             `json:"id"`
 	Title         string             `json:"title" bson:"title"`
 	Thumbnail     string             `json:"thumbnail" bson:"-"` // base64 encoding of bytearray
-	ThumbnailData []byte             `json:"-" bson:"thumbnailBin"`
+	ThumbnailData []byte             `json:"-" bson:"thumbnail"`
 	Description   string             `json:"description" bson:"description"`
 	Material      string             `json:"material" bson:"material"`
 	Price         float32            `json:"price" bson:"price"`
@@ -20,12 +26,49 @@ type ClimbingProduct struct {
 }
 
 type ClimbingVariant struct {
-	Variant   string `json:"variant" bson:"variant"`
-	Thumbnail string `json:"thumbnail" bson:"thumbnail"`
-	Color     string `json:"color" bson:"color"`
-	Inventory uint16 `json:"inventory" bson:"inventory"`
+	Variant       string `json:"variant" bson:"variant"`
+	Thumbnail     string `json:"thumbnail" bson:"-"` // base64 encoding of bytearray
+	ThumbnailData []byte `json:"-" bson:"thumbnail"`
+	Color         string `json:"color" bson:"color"`
+	Inventory     uint16 `json:"inventory" bson:"inventory"`
 }
 
-type BoardProduct struct {
-	Product `bson:",inline"`
+func (p *ClimbingProduct) Serializer() error {
+	if err := p.Product.Serializer(); err != nil {
+		return err
+	}
+
+	for _, variant := range p.Variants {
+		variant.Serializer()
+	}
+
+	return nil
+}
+
+func (p *ClimbingVariant) Serializer() error {
+	// get Thumbnail base64
+	i, err := img.FromBytes(p.ThumbnailData)
+	if err != nil {
+		return err
+	}
+	p.Thumbnail = i.Base64()
+	return nil
+
+}
+
+func (p *Product) Serializer() error {
+	// get Thumbnail base64
+	i, err := img.FromBytes(p.ThumbnailData)
+	if err != nil {
+		return err
+	}
+	p.Thumbnail = i.Base64()
+
+	// get id
+	if p.ObjectID == primitive.NilObjectID {
+		return errors.New("nil objectID")
+	}
+	p.ID = p.ObjectID.Hex()
+
+	return nil
 }
