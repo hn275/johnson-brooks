048c1041a2c3afeadf5266edc95a594c9f244052
hal <haln_01@proton.me>
2023-05-16 19:57:20 -0700

    feat: get all product endpoint

--- api/mod/product/product.go
+++ api/mod/product/product.go
@@ -1,10 +1,52 @@
 package product
 
 import (
+	"context"
+	"encoding/json"
+	"jb/database"
+	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-func T(w http.ResponseWriter, r *http.Request) {
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+
+	var product []database.Product
+	if err := getAllProducts(&product); err != nil {
+		panic(err)
+	}
+
+	for i := range product {
+		if err := product[i].Serializer(); err != nil {
+			panic(err)
+		}
+	}
+
+	log.Println("lasjdflkjasdf")
+	for _, p := range product {
+		log.Println(p.ID)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(&product); err != nil {
+		panic(err)
+	}
+}
+
+func getAllProducts(products *[]database.Product) error {
+	db := database.New()
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), database.Timeout)
+	defer cancel()
+
+	trx := db.Collection(database.Hangboards)
+	cur, err := trx.Find(ctx, bson.D{})
+	if err != nil {
+		return err
+	}
+
+	return cur.All(ctx, products)
 }
