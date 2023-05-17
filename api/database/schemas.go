d250a2e4f98307a75b94d5f4cf46adde8130ebc8
hal <haln_01@proton.me>
2023-05-17 13:22:04 -0700

    chore: generate multiple variants

--- api/database/schemas.go
+++ api/database/schemas.go
@@ -9,23 +9,16 @@ import (
 
 // PRODUCT
 type Product struct {
-	ObjectID      primitive.ObjectID `json:"-" bson:"_id,omitempty"`
-	ID            string             `json:"id" bson:"-"`
-	Title         string             `json:"title" bson:"title"`
-	Thumbnail     string             `json:"thumbnail" bson:"-"` // base64 encoding of bytearray
-	ThumbnailData []byte             `json:"-" bson:"thumbnail"`
-	Description   string             `json:"description" bson:"description"`
-	Material      string             `json:"material" bson:"material"`
-	Price         float32            `json:"price" bson:"price"`
-	Inventory     uint16             `json:"inventory" bson:"inventory"`
+	ObjectID    primitive.ObjectID `json:"-" bson:"_id,omitempty"`
+	ID          string             `json:"id" bson:"-"`
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	Material    string             `json:"material" bson:"material"`
+	Price       float32            `json:"price" bson:"price"`
+	Variants    []ProductVariant   `json:"variants" bson:"variants"`
 }
 
-type ClimbingProduct struct {
-	Product  `bson:",inline"`
-	Variants []ClimbingVariant `json:"variants" bson:"variants"`
-}
-
-type ClimbingVariant struct {
+type ProductVariant struct {
 	Variant       string `json:"variant" bson:"variant"`
 	Thumbnail     string `json:"thumbnail" bson:"-"` // base64 encoding of bytearray
 	ThumbnailData []byte `json:"-" bson:"thumbnail"`
@@ -33,36 +26,19 @@ type ClimbingVariant struct {
 	Inventory     uint16 `json:"inventory" bson:"inventory"`
 }
 
-func (p *ClimbingProduct) Serializer() error {
-	if err := p.Product.Serializer(); err != nil {
-		return err
-	}
-
-	for _, variant := range p.Variants {
-		variant.Serializer()
-	}
-
-	return nil
-}
-
-func (p *ClimbingVariant) Serializer() error {
-	// get Thumbnail base64
-	i, err := img.FromBytes(p.ThumbnailData)
-	if err != nil {
-		return err
-	}
-	p.Thumbnail = i.Base64()
-	return nil
-
-}
-
 func (p *Product) Serializer() error {
-	// get Thumbnail base64
-	i, err := img.FromBytes(p.ThumbnailData)
-	if err != nil {
-		return err
+	// serializer id
+	p.ID = p.ObjectID.Hex()
+
+	// get Thumbnail base64 for all variants
+	for i := range p.Variants {
+		image, err := img.FromBytes(p.Variants[i].ThumbnailData)
+		if err != nil {
+			return err
+		}
+
+		p.Variants[i].Thumbnail = image.Base64()
 	}
-	p.Thumbnail = i.Base64()
 
 	// get id
 	if p.ObjectID == primitive.NilObjectID {
