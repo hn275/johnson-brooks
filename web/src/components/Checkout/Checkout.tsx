62d220a09735abff4b539cce6f010a4705dd78ff
BenReichwein <ben.basque@icloud.com>
2023-05-16 20:21:05 -0700

    Implemented json into design

--- web/src/components/Checkout/Checkout.tsx
+++ web/src/components/Checkout/Checkout.tsx
@@ -1,37 +1,57 @@
 import cx from "classnames";
-import { CART_STORAGE, CartStorage, cartItem } from "../Cart/store"
-import { useStore } from "@nanostores/react";
+import { CART_STORAGE } from "../Cart/store"
 import { useState, useEffect } from "react";
 
 export function Checkout() {
-  const show = useCartIndicator();
-  console.log(show)
+  const [loading, setLoading] = useState(true);
+  const [items, setItems] = useState<Object>({});
+  const [price, setPrice] = useState<Number>(0);
 
-  return (
-    <div className={cx("grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32 p-20")}>
+  useEffect(()=> {
+    const i = window.localStorage.getItem(CART_STORAGE);
+    if (i) {
+      const jsonItem = JSON.parse(i)
+      setItems(jsonItem)
+      Object.entries(jsonItem).map(([_, e])=> {
+        setPrice((prev) => prev + (e as any).unitPrice)
+      })
+    }
+
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  } else {
+    return (
+      <div className={cx("grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32 p-20")}>
         <div className={cx("px-4 pt-8")}>
             <p className={cx("text-xl font-medium")}>Order Summary</p>
             <p className={cx("text-gray-400")}>Check your items. And select a suitable shipping method.</p>
             <div className={cx("mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6")}>
-                <div className={cx("flex flex-col rounded-lg bg-white sm:flex-row")}>
-                    <img className={cx("m-2 h-24 w-28 rounded-md border object-cover object-center")} src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                    <div className={cx("flex w-full flex-col px-4 py-4")}>
-                        <span className={cx("font-semibold")}>Nike Air Max Pro 8888 - Super Light</span>
-                        <span className={cx("float-right text-gray-400")}>42EU - 8.5US</span>
-                        <p className={cx("text-lg font-bold")}>$138.99</p>
-                    </div>
-                </div>
-                <div className={cx("flex flex-col rounded-lg bg-white sm:flex-row")}>
-                    <img className={cx("m-2 h-24 w-28 rounded-md border object-cover object-center")} src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-                    <div className={cx("flex w-full flex-col px-4 py-4")}>
-                        <span className={cx("font-semibold")}>Nike Air Max Pro 8888 - Super Light</span>
-                        <span className={cx("float-right text-gray-400")}>42EU - 8.5US</span>
-                        <p className={cx("mt-auto text-lg font-bold")}>$238.99</p>
-                    </div>
-                </div>
+                {Object.entries(items).map(([_, item])=> (
+                  <div className={cx("flex flex-col rounded-lg bg-white sm:flex-row")}>
+                      <img className={cx("m-2 h-24 w-28 rounded-md border object-cover object-center")} src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                      <div className={cx("flex w-full flex-col px-4 py-4")}>
+                          <span className={cx("font-semibold")}>{item.productTitle}</span>
+                          <span className={cx("float-right text-gray-400")}>Quantity:
+                          <label htmlFor="years" className={cx("text-sm font-medium text-gray-900 dark:text-white")}>{item.quantity}</label>
+                          <select id="years" className={cx("bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-10")}>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                          </select>
+                          </span>
+                          <p className={cx("text-lg font-bold")}>${item.unitPrice}</p>
+                      </div>
+                  </div>
+                ))}
             </div>
         </div>
-           
+          
         <div className={cx("mt-10 bg-gray-50 px-4 pt-8 lg:mt-0")}>
             <p className={cx("text-xl font-medium")}>Payment Details</p>
             <p className={cx("text-gray-400")}>Complete your order by providing your payment details.</p>
@@ -47,32 +67,11 @@ export function Checkout() {
 
             <div className={cx("mt-6 flex items-center justify-between")}>
                 <p className={cx("text-sm font-medium text-gray-900")}>Total</p>
-                <p className={cx("text-2xl font-semibold text-gray-900")}>$408.00</p>
+                <p className={cx("text-2xl font-semibold text-gray-900")}>{price.toString()}</p>
             </div>
             <button className={cx("mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white")}>Place Order</button>
         </div>
-    </div>
-  );
-}
-
-function useCartIndicator() {
-  const [counter, setCounter] = useState<boolean>(false);
-  const items = useStore(cartItem);
-
-  useEffect(() => {
-    const cachedCart = window.localStorage.getItem(CART_STORAGE);
-    if (!cachedCart) return;
-
-    const items = JSON.parse(cachedCart) as CartStorage;
-    for (const [k, v] of Object.entries(items)) {
-      cartItem.setKey(k, v);
-    }
-  }, []);
-
-  useEffect(() => {
-    const cartEntries = Object.keys(items);
-    setCounter(() => cartEntries.length !== 0);
-  }, [items]);
-
-  return counter;
+      </div>
+    )
+  }
 }
