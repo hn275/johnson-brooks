8a09328e3c74efdc9711b3c729c41107d73ba61f
hal <haln_01@proton.me>
2023-05-18 23:23:00 -0700

    chore: modularize product section

--- web/src/modules/shop/mock/mock.ts
+++
@@ -1,9 +0,0 @@
-import monorail from "./monorail.json";
-
-export const mockFetch = () => {
-  return new Promise((res, _) => {
-    setTimeout(() => {
-      res(JSON.stringify(monorail));
-    }, 1000);
-  });
-};
